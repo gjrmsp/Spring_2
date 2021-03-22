7da044736df4ce5d428d03b97763911515e90887
gjrmsp <gjrmsp@naver.com>
2021-03-23 01:32:56 +0900

    update

--- src/main/java/com/google/s2/robot/Arm_Left.java
+++ src/main/java/com/google/s2/robot/Arm_Left.java
@@ -1,14 +1,5 @@
 package com.google.s2.robot;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
-@Component
-@Qualifier("al")
 public class Arm_Left extends Arm {
-
-	public Arm_Left() {
-		this.setName("Left Arm");
-	}
 	
 }
--- src/main/java/com/google/s2/robot/Arm_Right.java
+++ src/main/java/com/google/s2/robot/Arm_Right.java
@@ -1,12 +1,5 @@
 package com.google.s2.robot;
 
-import org.springframework.stereotype.Component;
-
-@Component("ar")
 public class Arm_Right extends Arm {
-
-	public Arm_Right() {
-		this.setName("Right Arm");
-	}
 	
 }
